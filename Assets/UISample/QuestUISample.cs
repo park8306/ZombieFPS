8184f127b00e09e58b32b25048f3f358203406e4
park8306 <kosaf0804@gmail.com>
2021-07-23 23:17:44 +0900

    초기 커밋

---
+++ Assets/UISample/QuestUISample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UISample
+{
+    public class QuestUISample : BaseUI<QuestUISample>
+    {
+        new private void OnEnable()
+        {
+            base.OnEnable();
+            ToastMessage.Instance.ShowToast("퀘스트 UI 열림");
+        }
+
+        new private void OnDisable()
+        {
+            base.OnDisable();
+            if (ToastMessage.ApplicationQuit)
+                return;
+            ToastMessage.Instance.ShowToast("퀘스트 UI 닫힘");
+        }
+    }
+}
---
+++ Assets/UISample/UISampleTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UISample
+{
+    public class UISampleTest: MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                ChatUISample.Instance.Show();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                ChatUISample.Instance.Close();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                QuestUISample.Instance.Show();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                QuestUISample.Instance.Close();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                HistoryUI.ShowPreviousMenu();
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                MonobehaviorSingtonSample.Instance.MyPoint++;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                MonobehaviorSingtonSample.Instance.SomeMethod();
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                CSharpSingtonSample.Instance.MyPoint++;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                CSharpSingtonSample.Instance.SomeMethod();
+            }
+        }
+    }
+}
